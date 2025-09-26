2298e50c524fa9ca37c692a14660dd1b5fe9773b
KevinDeBenedetti <104367175+KevinDeBenedetti@users.noreply.github.com>
2025-09-26 15:00:57 +0200

    feat: ajouter une description au schéma de validation du groupe

--- shared/types/group.ts
+++ shared/types/group.ts
@@ -5,7 +5,9 @@ export const CreateGroupDtoSchema = z.object({
     .string()
     .min(3, { message: 'Le nom doit faire au moins 3 caractères' })
     .max(10, { message: 'Le nom doit faire au maximum 10 caractères' })
-    .regex(/^[\p{L}\p{N}\p{P}\p{S}\p{Zs}]+$/u, { message: 'Caractères invalides' }),
+    .regex(/^[\p{L}\p{N}\p{P}\p{S}\p{Zs}]+$/u, { message: 'Caractères invalides' })
+    .describe('Le nom du groupe'),
+    // Couvre quasiment tout ce qui est "imprimable" (hors contrôles comme \n, \t, etc.)
 })
 
 export type CreateGroupDtoType = z.infer<typeof CreateGroupDtoSchema>
