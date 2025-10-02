656deacd4ab3038fc27b967968d6a0673020594e
ArnaudTA <33383276+ArnaudTA@users.noreply.github.com>
2025-10-02 23:22:44 +0200

    fix: add missing env vars in example

--- server/api/v1/groups/list.get.ts
+++ server/api/v1/groups/list.get.ts
@@ -3,10 +3,11 @@ import repo from '../../../repository'
 export default defineEventHandler(async (event): Promise<ListGroupDtoType> => {
   // Search logic here
   const userId = event.context.user.sub
-  const { invited, joined } = await repo.listGroupsForUser(userId)
+  const { invited, joined, requested } = await repo.listGroupsForUser(userId)
 
   return {
     joined,
     invited,
+    requested,
   }
 })
--- shared/ListGroupSchema.ts
+++ shared/ListGroupSchema.ts
@@ -2,8 +2,9 @@ import { z } from 'zod'
 import GroupDtoSchema from './GroupSchema.js'
 
 export const ListGroupDtoSchema = z.object({
-  joined: GroupDtoSchema.omit({ members: true, invites: true }).array(),
-  invited: GroupDtoSchema.omit({ members: true, invites: true }).array(),
+  joined: GroupDtoSchema.omit({ members: true, invites: true, requests: true }).array(),
+  invited: GroupDtoSchema.omit({ members: true, invites: true, requests: true }).array(),
+  requested: GroupDtoSchema.omit({ members: true, invites: true, requests: true }).array(),
 })
 export type ListGroupDtoType = z.infer<typeof ListGroupDtoSchema>
 
