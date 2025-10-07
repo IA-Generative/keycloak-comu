307050e064a5f8e318aab5aaee9909bdfa05f783
ArnaudTa <33383276+ArnaudTA@users.noreply.github.com>
2025-10-08 00:57:45 +0200

    fix: enhance delete group functionality with confirmation prompt

--- app/components/DangerZone.vue
+++ app/components/DangerZone.vue
@@ -20,7 +20,14 @@ const canLeaveGroup = computed(() => {
   return false
 })
 
-async function deleteGroup() {
+type Action = 'idle' | 'pendingDelete' | 'deleting'
+const actionInProgress = ref<Action>('idle')
+
+async function deleteGroup(confirm: boolean) {
+  if (!confirm) {
+    actionInProgress.value = 'pendingDelete'
+    return
+  }
   const data = await fetcher(`/api/v1/groups/delete`, {
     method: 'post',
     body: { groupId: props.group.id },
@@ -44,8 +51,41 @@ async function leaveGroup() {
     v-if="mylevel > 0"
     title="Zone de danger"
     title-tag="h3"
+    class="relative"
   >
-    <div class="flex flex-col gap-12 items-start fr-mt-4w">
+    <div
+      v-if="actionInProgress === 'pendingDelete'"
+      class="fr-mb-2w flex flex-col justify-center items-center gap-2 w-full h-full absolute top-0 left-0 p-4 rounded-lg shadow-lg z-10"
+      :class="{
+        [actionInProgress]: true,
+      }"
+    >
+      <p class="fr-mb-0 text-wrap text-red-600 font-bold text-center">
+        Êtes-vous sûr de vouloir supprimer ce groupe ?<br>
+        Toutes les ressources du groupe seront supprimées.<br>
+        Cette action est irréversible.
+      </p>
+      <div class="flex flex-row gap-2">
+        <DsfrButton
+          secondary
+          title="Annuler la suppression"
+          @click="() => { actionInProgress = 'idle' }"
+        >
+          Annuler
+        </DsfrButton>
+        <DsfrButton
+          class="bg-red-600 border-red-600 hover:bg-red-700 hover:border-red-700"
+          title="Confirmer la suppression"
+          @click="() => deleteGroup(true)"
+        >
+          Confirmer
+        </DsfrButton>
+      </div>
+    </div>
+    <div
+      class="flex flex-col gap-12 items-start fr-mt-4w"
+      :class="{ 'blur-sm': actionInProgress !== 'idle' }"
+    >
       <div class="flex flex-row justify-between gap-4 w-full">
         <div>
           <h6>Quitter le groupe</h6>
@@ -76,9 +116,15 @@ async function leaveGroup() {
           label="Supprimer le groupe"
           :disabled="!amIOwner"
           secondary
-          @click="deleteGroup"
+          @click="deleteGroup(false)"
         />
       </div>
     </div>
   </DsfrCallout>
 </template>
+
+<style>
+.pendingDelete {
+  background-color: color-mix(in srgb, var(--background-alt-grey), transparent 25%);
+}
+</style>
